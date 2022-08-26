a542256d6d24d50f9965eb7f886e5d3da2e9192e
timf34 <farrelti@tcd.ie>
2022-08-26 18:57:02 +0100

    mach 1 works - errors with json file

--- gap_lead_data.py
+++ gap_lead_data.py
@@ -34,22 +34,93 @@ def print_pandas_dataframe_info(dataframe: pandas.DataFrame) -> None:
 
     print("We will now print the some random values 1000 rows into the dataframe object, after it has been sorted by"
           "Time")
-    print("dataframe sorted by Time: \n", dataframe.sort_values(by='Time')[1000:1040], "\n")
+    print("dataframe sorted by Time: \n", dataframe.sort_values(by='Time')[3000:3040], "\n")
 
 
-def get_timing_data() -> Tuple[pandas.DataFrame, pandas.DataFrame]:
+def get_timing_data(sorted: bool = True) -> pandas.DataFrame:
     """
     # https://theoehrly.github.io/Fast-F1/api.html#fastf1.api.timing_data
     :return:
-        (DataFrame, DataFrame): The first DataFrame is the lap data, the second is the stream_data (with gap times!)
+        ~~(DataFrame, DataFrame): The first DataFrame is the lap data, the second's the stream_data (with gap times!)~~
+        DataFrame: We will just return the streaming data actually to make things easier for now
 
     """
-    return fastf1.api.timing_data(session.api_path)
+    if sorted:
+        print("Getting sorted timing data")
+        return fastf1.api.timing_data(session.api_path)[1].sort_values('Time')[1000:1040]
+    else:
+        print("Getting unsorted timing data")
+        return fastf1.api.timing_data(session.api_path)[1]
+
+
+def iterate_over_timing_data(stream_data: pandas.DataFrame, file = None) -> None:
+    """
+    This function will iterate over the timing data... getting tired, will come back to this tmrw.
+    """
+    stats = {}
+
+    less_than_half: bool = True  # This variable keeps track of whether we have changed to the next half second or not.
+    last_state: bool = less_than_half  # This just keeps track of the last value of less_than_half.
+
+    for index, row in stream_data.iterrows():
+        value = (row['Time'].total_seconds() * 10) % 10
+        if value < 5:
+            less_than_half = True  # We are in the first half second of the second.
+        else:
+            less_than_half = False
+
+        if less_than_half != last_state:
+            # We have changed to the next half second.
+            # Do logic here
+            print("We have changed to the next half second.")
+            last_state = less_than_half
+            if file is not None:
+                json.dump(stats, file, indent=4)
+            stats = {}  # Reset the stats dictionary.
+
+        # Fill in the stats dictionary
+        # Just use the last timestep before the .5 and .0 mark for now, but come back to clean this up.
+        # We want the timesteps to be in 0.5 intervals, and also to allow for cases where the gap between timestamps
+        # is greater than 1 second.
+        stats["timestamp"] = row['Time'].total_seconds()
+        stats["car_number"] = row['Driver']
+        stats["gap_to_leader"] = row['GapToLeader']
+        stats["gap_to_postiion_ahead"] = row['IntervalToPositionAhead']
+
+
+
+        # print(row)
+        # stats["timestamp"] = row['Time'].total_seconds()
+        # print("Here is the row \n", row)
+        # print("Here is the index\n", index)
+        # count += 1
+        # if int((row['Time'].total_seconds() * 10) % 10) <= 4:  # This would exclude all times between 0.5 and 0.999 rn
+        #     # Note that by placing this here, we would still get the car that is the first one to be greater than 0.5
+        #     # which is a bit of an issue. I am sure there is a smarter way to do this, but I will leave it like this for now
+        #     # Note that I should also include the real timesteps in the dictionary to help with debugging.
+        #     print(stats)
+        #     stats = {}
+
+
+    print(stats)
+
+
+def write_to_json_file(filename: str, stream_data: pandas.DataFrame) -> None:
+    """
+    This function will write the data to a json file.
+    :param filename:
+    :return:
+    """
+    with open(filename, 'w') as f:
+        iterate_over_timing_data(stream_data, f)
+
 
 
 def main():
-    lap_data, stream_data = get_timing_data()
-    print_pandas_dataframe_info(stream_data)
+    stream_data = get_timing_data()
+    # print_pandas_dataframe_info(stream_data)
+    # iterate_over_timing_data(stream_data)
+    write_to_json_file('stream_data.json', stream_data)
 
 
 if __name__ == '__main__':
