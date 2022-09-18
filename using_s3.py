9c764320c1be50d3c6b6f860f49c6329656ab054
timf34 <farrelti@tcd.ie>
2022-09-18 22:30:00 +0200

    still messy but sending MQTT messages to the MQTT 'server'

--- using_s3.py
+++ using_s3.py
@@ -2,6 +2,8 @@ import boto3
 import json
 import sys
 
+from typing import Dict
+
 ACCESS_KEY = 'AKIAXJHR7VFKNE7DRJF7'
 SECRET_ACCESS_KEY = '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'
 
@@ -10,10 +12,11 @@ class StreamFromS3:
     def __init__(self):
         self.s3_url: str = 'https://testbucket10022022.s3.eu-west-1.amazonaws.com/very_short_stream_data.json'
         self.s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_ACCESS_KEY)
+        self.iot_client = boto3.client('iot-data', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_ACCESS_KEY, region_name='eu-west-1')
         self.key = "stream_data.json"
         self.bucket = "testbucket10022022"
 
-    def get_data(self):
+    def get_data(self) -> Dict:
         return self.s3.get_object(Bucket=self.bucket, Key=self.key)
 
     def print_data(self):
@@ -45,10 +48,34 @@ class StreamFromS3:
         json_object = json.loads(dict_object)
         print("json_object type: ", type(json_object))
 
+    def parse_data(self):
+        data = self.get_data()
+        json_object = json.loads(data['Body'].read().decode('utf-8'))
+        print(json_object["streaming_data"][0].keys())
+
+        for count, info in enumerate(json_object["streaming_data"]):
+            print(f"count: {count} i: {info}")
+
+            response = self.iot_client.publish(
+                topic="PreMatch/1",
+                qos=1,
+                payload=str(info)
+            )
+
+            if count == 5:
+                break
+
+    # TODO: do this line locally `iotClient = client = boto3.client('iot-data', region_name='eu-west-1')` and publish
+
+    @staticmethod
+    def _pass():
+        pass
+
+
 
 
 if __name__ == '__main__':
     stream_from_s3 = StreamFromS3()
     # stream_from_s3.get_data()
-    stream_from_s3.print_data()
-
+    # stream_from_s3.print_data()
+    stream_from_s3.parse_data()
