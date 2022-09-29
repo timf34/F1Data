ca34eb0f870a95f213be88f8e9b21751e8f726a7
timf34 <farrelti@tcd.ie>
2022-09-29 16:43:32 -0400

    most recent lap time code works - we now need to add it to a json file though

--- most_recent_lap_time.py
+++ most_recent_lap_time.py
@@ -1,9 +1,11 @@
 from copy import deepcopy
 import pandas
-from typing import Dict, Tuple, List
+from typing import Dict, Tuple, List, Union
 import fastf1
 import fastf1.plotting
 import json
+
+from utils import load_json_file
 
 fastf1.plotting.setup_mpl()
 
@@ -18,9 +20,16 @@ class MostRecentLapTime:
         if RUN_SESSION:
             self.session.load()
 
+        self.list_of_car_numbers = ['44', '77', '33', '5', '16', '10', '20', '55', '26', '8',
+                                    '23', '3', '27', '7', '11', '99', '63', '88', '18', '4']
+        # Lap data
         self.laps = self.session.load_laps()
+        self.lap_data_per_car: Dict[str, pandas.DataFrame] = self.create_dict_with_pandas_dataframes()
 
-    def check_if_lap_data_is_equal(self, car_number: str) -> pandas.DataFrame:
+        self.short_json_file: str = "data/stream_data_short_keys.json"
+
+
+    def check_if_lap_data_is_equal(self, car_number: str) -> None:
         """
             This function takes a car number as an argument, then returns the data for that car.
             :param
@@ -34,12 +43,75 @@ class MostRecentLapTime:
         print(dir(self.session))
         print("Are these equal? ", pandas.DataFrame.equals(self.session.laps, self.session.load_laps()))
 
+    @staticmethod
+    def iterable_through_json_file(file_path: str) -> Dict[str, Dict[str, Dict[str, Union[int, str]]]]:
+        """
+            This function takes a file path as an argument, then iterates through the file.
+            :param
+                file_path: str
+                    This is a string that represents the file path.
+            :return:
+        """
+        # This function could be an iterator -> yield
+
+        with open(file_path) as file:
+            data = json.load(file)
+            for count, i in enumerate(data["streaming_data"]):
+                yield i
+
+    def create_dict_with_pandas_dataframes(self) -> Dict[str, pandas.DataFrame]:
+        # Note: this only returns an iterator!
+        return {i: self.laps.pick_driver(i).iterrows() for i in self.list_of_car_numbers}
+
+    def iterate_through_json_file(self):
+
+        # Initialize the first lap time for each car (i.e. get the first row)
+        lap_data = {i: next(self.lap_data_per_car[i]) for i in self.list_of_car_numbers}
+
+        # This iterates through the og dataset over each dict containing ("ts" and "car_number") keys
+        for i in self.iterable_through_json_file(self.short_json_file):
+            car_timestamp = i["ts"]
+            print("timestamp: ", car_timestamp)
+
+
+            for car_num in self.list_of_car_numbers:
+                # print(car_num, i["car_number"][car_num])
+                # print("lap data: ", lap_data[car_num][1]["LapTime"])
+
+                lap_timestamp = lap_data[car_num][1].Time.total_seconds()
+                print("lap timestamp: ", lap_timestamp)
+                i["car_number"][car_num]["mostRecentLapTime"] = lap_data[car_num][1]["LapTime"].total_seconds()
+                print("here is i car number: ", i["car_number"][car_num])
+
+                if lap_timestamp <= car_timestamp:
+                    print("yes it is less than")
+                    try:
+                        lap_data[car_num] = next(self.lap_data_per_car[car_num])
+                    except StopIteration:
+                        print("StopIteration - we are done with this car")
+                        pass
+
+    def make_our_json_file(self, dict_arg: Dict[str: Union[int, Dict[str, Union[int, str]]]]) -> None:
+        """
+            This function takes a dict as an argument within the structure of the dicts within our List for the main
+            json file -> {"ts" : 123, "car_number" : {"44": {...}, "77": {...}, ...}}
+
+            We add these dicts to a List which makes up our {"streaming_data: []"} key.
+
+            We then create a json file.
+        """
+
+        big_list = [] # This is the list that will be the value for the "streaming_data" key
+
+
+
+
+
 
 
 def main():
     most_recent_lap_time = MostRecentLapTime()
-    laps = most_recent_lap_time.laps
-
+    most_recent_lap_time.iterate_through_json_file()
 
 
 
