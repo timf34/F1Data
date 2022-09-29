f32a62b5914cbaeecc4d68cf43192ec90fa8131c
timf34 <farrelti@tcd.ie>
2022-09-29 01:10:36 -0400

    random code snippet

--- learning/rando.py
+++ learning/rando.py
@@ -1,15 +1,30 @@
-sample_dict = {}
-big_list = []
+#sample_dict = {}
+# big_list = []
+#
+# sample_dict = {"a": 1, "b": 2, "c": 3}
+#
+# for key, value in sample_dict.items():
+#     print(key, value)
+#     big_list.append(sample_dict.copy())
+#     sample_dict["a"] += 3
+#     sample_dict["b"] += 3
+#     sample_dict["c"] += 3
+#     print(big_list)
+#
+#
+# print(big_list)
 
-sample_dict = {"a": 1, "b": 2, "c": 3}
+# For loop that increments by the value of 0.5
 
-for key, value in sample_dict.items():
-    print(key, value)
-    big_list.append(sample_dict.copy())
-    sample_dict["a"] += 3
-    sample_dict["b"] += 3
-    sample_dict["c"] += 3
-    print(big_list)
+for i in range(0, 10, 1):
+    if i == 0:
+        pass
+    else:
+        print(i/2)
 
+starting = 2001.5
+enidng = 2004.5
 
-print(big_list)
+# increment between starting and ending in 0.5 increments
+for i in range(int(starting*2 + 1), int(enidng*2)):
+    print(i/2)
