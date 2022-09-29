9bfadc5b1f3ae77ef6ce71d6a1c7b07d8ddb1008
timf34 <farrelti@tcd.ie>
2022-09-29 15:22:30 -0400

    misc code snippets

--- learning/rando.py
+++ learning/rando.py
@@ -1,4 +1,6 @@
-#sample_dict = {}
+import json
+
+# sample_dict = {}
 # big_list = []
 #
 # sample_dict = {"a": 1, "b": 2, "c": 3}
@@ -16,15 +18,36 @@
 
 # For loop that increments by the value of 0.5
 
-for i in range(0, 10, 1):
-    if i == 0:
-        pass
-    else:
+def trying_for_loops():
+    for i in range(0, 10, 1):
+        if i == 0:
+            pass
+        else:
+            print(i/2)
+
+    starting = 2001.5
+    enidng = 2004.5
+
+    # increment between starting and ending in 0.5 increments
+    for i in range(int(starting*2 + 1), int(enidng*2)):
         print(i/2)
 
-starting = 2001.5
-enidng = 2004.5
 
-# increment between starting and ending in 0.5 increments
-for i in range(int(starting*2 + 1), int(enidng*2)):
-    print(i/2)
+def trying_to_parse_json_file_by_time():
+    # Open json file
+    with open(r"C:\Users\timf3\PycharmProjects\f1Data\data\stream_data_short_keys.json", "r") as file:
+        data = json.load(file)
+        list_of_data = data["streaming_data"]
+
+        # Note that the start time effectively starts from 1993 seconds!
+
+        list_of_data = list_of_data[0:15]
+
+        for count, data in enumerate(list_of_data):
+            print(f"count: {count} data: {data}")
+
+def main():
+    trying_to_parse_json_file_by_time()
+
+if __name__ == "__main__":
+    main()
