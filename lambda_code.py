1ee83131ad4ffb2348331f9ab7aa484cc129cef1
timf34 <farrelti@tcd.ie>
2022-09-30 13:15:41 -0400

    checkpoint of lambda code

--- lambda_code.py
+++ lambda_code.py
@@ -3,14 +3,16 @@ import boto3
 import time
 from typing import Dict
 
-# This is just a copy of the lambda code before I went and changed it...
+# This is a copy of the Lambda code - it is working! 30/9/22
+
 
 class StreamFromS3:
-    def __init__(self, topic="1"):
+    def __init__(self, file_name: str, topic="1"):
         self.s3_url: str = 'https://testbucket10022022.s3.eu-west-1.amazonaws.com/very_short_stream_data.json'
         self.s3 = boto3.client('s3')
         self.iot_client = boto3.client('iot-data', region_name='eu-west-1')
-        self.key: str = "stream_data.json"
+        # self.key: str = "new_json_file.json"
+        self.key: str = file_name
         self.bucket: str = "testbucket10022022"
         self.static_info_key: str = "static_new_json_file.json"
         self.short_loop: bool = True
@@ -47,28 +49,28 @@ class StreamFromS3:
         json_object = json.loads(data['Body'].read().decode('utf-8'))
 
         for count, info in enumerate(json_object["streaming_data"]):
-            # print(f"count: {count} i: {info}")
+            print(f"count: {count} i: {info}")
             self.publish_to_arduino(info)
             time.sleep(0.5)
 
             if self.short_loop:
-                if count == 5:
+                if count == 240:
                     break
 
     def publish_to_arduino(self, data: Dict):
-        print("topic", self.topic)
         response = self.iot_client.publish(
-            topic="RACE/1",
+            topic=self.topic,
             qos=1,
-            payload="hey there"
+            payload=str(data)
         )
+        print(response)
 
 
 def lambda_handler(event, context):
-    topic = event["Requested Topic"]
-    topic = topic.replace(".json", "")  # Remove '.json' from string.
+    req_topic = event["Requested Topic"]
+    topic = req_topic.replace(".json", "")  # Remove '.json' from string.
 
-    stream_from_s3 = StreamFromS3(topic)
+    stream_from_s3 = StreamFromS3(topic=topic, file_name=req_topic)
     stream_from_s3.parse_data()
 
     return ({"data": topic})
